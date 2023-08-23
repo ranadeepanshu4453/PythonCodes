371a2ec90535bb15b2ebd4f40512212421ace726
Deepanshu Chauhan <115064563+ranadeepanshu4453@users.noreply.github.com>
2023-08-24 00:26:50 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,28 @@
+# def calculate(a,b):
+#     return a*b
+
+# for c in range(3):
+#     a=int(input("enter first number::"))
+#     b=int(input("enter second number::"))
+#     print(calculate(a,b))
+    
+# def fun(a,b):
+#     return a if(a>b) else b
+
+# print(fun(50,6))
+
+# converting into tuple
+
+# def fun(*num):
+#     print(num)
+    
+# fun(1,2,3,4,5,6)
+
+# converting into dictionary
+
+# def fun(**dict):
+#     print(type(dict))
+#     print(dict)
+
+# fun(name="deepanshu",age=25,city="sre")
+
